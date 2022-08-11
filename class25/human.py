4010cd77e2d3fd7c70fdeb4c8966a0b1e39219d1
Abdullah Jaffrey <96979542+AbdullahJaffrey@users.noreply.github.com>
2022-08-11 21:55:10 +0500

    ClassCode

---
+++ class25/human.py
@@ -0,0 +1,16 @@
+class Human():
+    
+    def __init__(self, age, blood_group):
+        
+        self.age = age
+        
+        self.blood_group = blood_group
+
+    def blood_type(self):
+
+         return f'Your blood type is {self.blood_group}! '
+
+    def donated(self):
+
+          pass
+        
